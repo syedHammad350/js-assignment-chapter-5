b395875f297a67763e231b3a76906d5963202711
syedHammad350 <ahmed22ali456@gmail.com>
2024-09-28 23:21:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+// chapter 1
+
+alert("welocme to js land...Happy coding!");
+alert("welcome to js");
+alert("Happy coding");
+console.log("hello");
+
+// chapter 2
+
+var username=("sami")
+var myname= ("syed hammad")
+var message=("hello world")
+alert(message) 
+var studentname=("Ali")
+var studentage=("17 year old")
+var studentclasification=("1st year")
+alert(studentname)
+alert(studentage)
+alert(studentclasification)
+var email=("syedali22@gmail.com")
+alert("my email address is "+email)
+var book=("A smarter way to learn javascript")
+alert(book)
+console.log("yeh! I can write HTML content through javascript")
+
+// chapter 3
+
+var age=(" i am 17 year old")
+alert(age)
+var visit=("you have visited the site 14 time")
+alert(visit)
+var birthyear=("23 oct")
+alert(birthyear)
+var visitior=("abdullah")
+var quantity=("8")
+var productname=("T-shirt")
+console.log(visitior +  quantity + productname)
+
+// chapter 4
+
+// QUESTION 1
+
+var A=5,B=6,C=7
+
+// QUESTION 2
+
+// LEGAL VARAIBLE
+
+var d = "6";
+var $e = 10;
+var first_name = hammad;
+var scondname = ali;
+
+
+//ILLEGAL VARIABLES
+// var 2xyz=HR
+// var first name=HR
+// var console.log =HR
+// var hamm@d =HR
+
+// chapter 5
+
+console.log("sum of 3 and 5 is to 8")
+console.log("4*1=4")
+console.log("4*2=8")
+console.log("4*3=12")
+console.log("4*4=16")
+console.log("4*5=20")
+console.log("4*6=24")
+console.log("4*7=28")
+console.log("4*8=32")
+console.log("4*9=36")
+console.log("4*10=40")
